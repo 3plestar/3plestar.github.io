337f54fdb9e1f6cd0de8c66f5e52e553e49edb08
David <David_dekker@live.nl>
2022-12-21 11:21:24 +0100

    Complete overhaul

--- resources/navigation.js
+++ resources/navigation.js
@@ -1,29 +1,30 @@
+var offset = 100;
 
 function scrollWie() {
     var rect = document.getElementById('wie').getBoundingClientRect();
-    window.scrollTo(0, rect.top+window.scrollY-50);
+    window.scrollTo(0, rect.top+window.scrollY-offset);
 }
 
 function scrollPort() {
     var rect = document.getElementById('port').getBoundingClientRect();
-    window.scrollTo(0, rect.top+window.scrollY-50);
+    window.scrollTo(0, rect.top+window.scrollY-offset);
 }
 
 function scrollVis() {
     var rect = document.getElementById('vis').getBoundingClientRect();
-    window.scrollTo(0, rect.top+window.scrollY-50);
+    window.scrollTo(0, rect.top+window.scrollY-offset);
 }
 
 function scrollCont() {
     var rect = document.getElementById('cont').getBoundingClientRect();
-    window.scrollTo(0, rect.top+window.scrollY-50);
+    window.scrollTo(0, rect.top+window.scrollY-offset);
 }
 
 function toggleNav() {
-    if(document.getElementsByClassName('navBar')[1].style.display== "none"|| document.getElementsByClassName('navBar')[1].style.display== ""){
-        document.getElementsByClassName('navBar')[1].style.display= "grid";
+    if(document.getElementsByClassName('navBG')[0].style.display== "none"|| document.getElementsByClassName('navBG')[0].style.display== ""){
+        document.getElementsByClassName('navBG')[0].style.display= "flex";
     }
     else{
-        document.getElementsByClassName('navBar')[1].style.display= "none";
+        document.getElementsByClassName('navBG')[0].style.display = "";
     }
 }
